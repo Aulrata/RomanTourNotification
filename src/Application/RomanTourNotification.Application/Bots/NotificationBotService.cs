46147bd48d95fbd53f61bef0447388ad52077d28
Roman <Aulrata@gmail.com>
2025-02-03 00:54:53 +0300

    test: test bot

--- src/Application/RomanTourNotification.Application/Bots/NotificationBotService.cs
+++ src/Application/RomanTourNotification.Application/Bots/NotificationBotService.cs
@@ -11,6 +11,17 @@ public class NotificationBotService : INotificationBotService
 
     public async Task StartAsync(ITelegramBotClient botClient, CancellationToken cancellationToken)
     {
+        while (true)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                break;
+
+            if (DateTime.UtcNow.Hour != 0 || DateTime.UtcNow.Minute != 57) continue;
+
+            await botClient.SendMessage(-1001844409797, "Запланированное сообщение", cancellationToken: cancellationToken);
+            await Task.Delay(61 * 1000, cancellationToken);
+        }
+
         await Task.CompletedTask;
     }
 }
