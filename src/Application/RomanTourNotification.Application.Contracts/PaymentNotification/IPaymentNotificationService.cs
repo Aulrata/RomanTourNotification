a6447ceba08c4dcc312dbc55cf3c8101d3915cbf
Roman <Aulrata@gmail.com>
2025-05-03 00:16:57 +0300

    feat: split payment notice

--- src/Application/RomanTourNotification.Application.Contracts/PaymentNotification/IPaymentNotificationService.cs
+++ src/Application/RomanTourNotification.Application.Contracts/PaymentNotification/IPaymentNotificationService.cs
@@ -1,8 +1,11 @@
 using RomanTourNotification.Application.Models.EnrichmentNotification;
+using System.Text;
 
 namespace RomanTourNotification.Application.Contracts.PaymentNotification;
 
 public interface IPaymentNotificationService
 {
-    public Task<string> GetPaymentMessageAsync(DateDto dateDto, CancellationToken cancellationToken);
+    public Task GetAllPaymentMessagesAsync(DateDto dateDto, StringBuilder sb, CancellationToken cancellationToken);
+
+    public Task GetPaymentMessageByManagerAsync(DateDto dateDto, StringBuilder sb, string managerFullname, CancellationToken cancellationToken);
 }
